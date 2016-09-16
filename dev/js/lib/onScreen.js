878f964de014514f77ea02e2440b9da7d28722a1
Martin, Glenn (DET-CEW) <Glenn.Martin@c-e.com>
2016-09-16 15:49:47 -0400

    onScreen lib function added

---
+++ dev/js/lib/onScreen.js
@@ -0,0 +1,29 @@
+/***
+ * onScreen
+ *
+ * Determines if element is on screen and within the bounds of the offsets.
+ *
+ * @param {DOMElement} el
+ * @param {number, 0-1, optional} on offset
+ *   multiplier of screen height to determine for when an object is truly "on
+ *   screen". Example, passing `.5` would mean the element isn't on screen until
+ *   its top is above the midpoint of the screen.
+ * @param {number, 0-1, optional} off offset
+ *   multiplier of screen height to determine for when an object is truly "off
+ *   screen". Example, passing `.1` would mean the element isn't off screen (passed)
+ *   until its bottom is above the 10% point of the screen.
+ *
+ * @returns {boolean}
+ */
+function onScreen (el, onOffset, offOffset) {
+  var clientRect = el.getBoundingClientRect();
+  if (isNaN(onOffset))
+    onOffset = 1;
+  if (isNaN(offOffset))
+    offOffset = 0;
+  return
+    clientRect.top < windowSize.height() * onOffset &&
+    clientRect.bottom > windowSize.height() * offOffset;
+}
+
+module.exports = onScreen;
